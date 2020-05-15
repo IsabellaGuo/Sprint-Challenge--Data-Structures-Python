3ba2e65be20130a90e73b5d41f1f80999b2c2817
IsabellaGuo <isabella.guofei@gmail.com>
2020-05-15 09:27:56 -0700

    reverse is done.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.buffer = []
+        self.index = 0
 
     def append(self, item):
-        pass
+        # If there is empty spot in the ring
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(item)
+        # If there is no spot in the ring
+        else:
+            if len(self.buffer) == self.capacity:
+               self.index
+
+
 
     def get(self):
         pass
